a6b7abece4e88a96f3082b7bb2464763c9578feb
mnyu <mnyu1017@gmail.com>
2023-10-01 21:31:14 +0900

    스프링부트 기본 - section3 (정률 할인 정책 적용)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,15 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolish;
 import hello.core.discount.FixDiscountPolish;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolish discountPolish = new FixDiscountPolish();
+    //    private final DiscountPolish discountPolish = new FixDiscountPolish();
+    private final DiscountPolish discountPolish = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
